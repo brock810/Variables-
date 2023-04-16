276b5f376546a7590b8014b72bf700ea77387436
brock810 <130688727+brock810@users.noreply.github.com>
2023-04-16 16:23:45 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace VariablesExercise
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            string catName;
+
+            catName = "Brock";
+
+            int catAge = 6;
+
+            char firstInitial = 'B';
+
+            bool isHungry = true;
+
+            double catWeight = 96.5;
+
+            decimal catHeight = 29.3m;
+
+            Console.WriteLine($"My cats name is {catName} and she, is only {catAge} years old");
+            Console.WriteLine($"He weighs {catWeight} Pounds and he is {catHeight} inches tall");
+            Console.WriteLine($"it is {isHungry} he is always hungry");
+        }
+    }
+
+}
+
+
